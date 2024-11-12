829af0c67b1249d5a3f8aa74d9b0afa21b7cd1f8
Santiago Mesa <thiagomesa21@gmail.com>
2024-11-11 19:05:39 -0500

    segundo commit
    en este commit terminamos el hero y las otras secciones.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Portfolio from './components/Portfolio'
+import Skills from './components/Skills'
+import Experience from './components/Experience'
+import Contact from './components/Contact'
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Portfolio />
+      <Skills />
+      <Experience />
+      <Contact />
+
     </>
   )
 }
--- src/components/ShinyEffect.jsx
+++ src/components/ShinyEffect.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 
 const ShinyEffect = ({left, right, top, size = 500}) => {
-  const positionStyles = {
-    top: `${top}px`,
-    width: `${size}px`,
-    height: `${size}px`,
-    zIndex: -1
-  }
 
-  if (left !== undefined) {
-    positionStyles.left = `${left}px`
-  }
+    const positionStyles = {
+        top:`${top}px`,
+        width:`${size}px`,
+        height:`${size}px`,
+        zIndex: -1
+    }
 
-  if (right !== undefined) {
-    positionStyles.right = `${right}px`
-  }
+    if (left !== undefined) {
+        positionStyles.left = `${left}px`
+    }
+
+    if (right !== undefined) {
+        positionStyles.right = `${right}px`
+    }
 
 
   return (
